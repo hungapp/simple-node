ee0442b4e8eccc55ebf9df82338be82ec34c9791
Hung Nguyen <viethung1996@gmail.com>
2020-05-18 16:54:15 -0400

    feat: Add console.log for favorite food

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log('Containers rule!');
     await sleep(5000);
   }
 }
 
 main();
+
+const favoriteFood = process.env.FAVORITE_FOOD;
+console.log(`My favorite food is ${favoriteFood}`);
